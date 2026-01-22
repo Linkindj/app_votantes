041cb5b06d1624667f076b7c9afb87eeb9a785c0
Oscar Romero <oscarromeroyanez@gmail.com>
2026-01-22 12:53:14 -0500

    Fix SQLAlchemy config for Railway

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = 'clave-super-secreta'
-    SQLALCHEMY_DATABASE_URI = os.getenv("MYSQL_URL").replace(
-        "mysql://", "mysql+pymysql://"
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv("MYSQL_URL")
+        .replace("mysql://", "mysql+pymysql://")
+        + "?ssl=true"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
