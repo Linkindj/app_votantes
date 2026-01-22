f60d4d1be505fab717ebb54c7e5f50465969c2f4
Oscar Romero <oscarromeroyanez@gmail.com>
2026-01-22 10:57:42 -0500

    Fix MySQL Railway connection

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "clave-super-secreta")
 
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("MYSQL_URL") or
-        "mysql+pymysql://root:@localhost/app_votantes"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("MYSQL_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
