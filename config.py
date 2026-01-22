c94a6ca8fe9bea3f1b63164c7f846fff48e2be85
Oscar Romero <oscarromeroyanez@gmail.com>
2026-01-22 13:45:32 -0500

    Fix SQLAlchemy config for Railway

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = (
         os.getenv("MYSQL_URL")
         .replace("mysql://", "mysql+pymysql://")
-        
+        print("MYSQL_URL:", os.getenv("MYSQL_URL"))
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
