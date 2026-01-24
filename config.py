03bb86b6b4e4270edbc7c1d51d7774c3ba8b1827
Oscar Romero <oscarromeroyanez@gmail.com>
2026-01-24 07:59:24 -0500

    arreglo de conexión

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-print("DB URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))
