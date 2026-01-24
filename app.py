159a1f90b405b8a2dbba3ac09103117246a62f23
Oscar Romero <oscarromeroyanez@gmail.com>
2026-01-24 07:40:56 -0500

    arreglo de conexión

--- app.py
+++ app.py
@@ -253,3 +253,5 @@ if __name__ == '__main__':
 
 with app.app_context():
     db.create_all()
+
+print("DB URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))
--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'clave-super-secreta'
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+print("DB URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))
